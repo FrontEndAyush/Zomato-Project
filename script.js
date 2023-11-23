15193848884c02c86a2bb56ca249d85742bc1e30
ayush <iamrealglider@gmail.com>
2023-11-24 00:02:52 +0530

    add image effect  on the interface of the website. you just move your mouse and you will see some amazing stuff it maybe annoying for you.

--- script.js
+++ script.js
@@ -4,137 +4,177 @@ const scroll = new LocomotiveScroll({
 });
 
 
-window.addEventListener("mousemove", (details) =>{
-    let circle = document.querySelector(".circle")
-    circle.style.transform = `translate(${details.clientX}px, ${details.clientY}px)`
-    })
+// window.addEventListener("mousemove", (details) =>{
+//     let circle = document.querySelector(".circle")
+//     circle.style.transform = `translate(${details.clientX}px, ${details.clientY}px)`
+//     })
 
-let currentIndexForHeader = 0
-let currentIndex = 0
 
-function automaticCustomerReviewChanger() {
-    let paragraph = document.getElementById("paragraph")
-    let imagefile = document.getElementById('imagefile')
-    let name = document.getElementById("name")
-    let profession = document.getElementById("profession")
 
-    let arrOfImages = ["secondpageImg/2.png", "secondpageImg/3.png", "secondpageImg/1.png"]
-    let arrOfParagraph = ["Earlier, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Later, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Getter, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato .",]
-    let arrOfNames = ["Jasmeet Singh", "Raghudeep Singh", "Pardeep Singh"]
-    let arrOfProfession = ["Owner - Pick and Move, Udaipur", "Owner - Run and Eat, Udaipur", "Owner - Lyrically and Musically, Udaipur"]
+// Throttling Function
+const throttleFunction = (func, delay) => {
 
-    if (currentIndex >= 2) {
-        currentIndex = 0
+    // Previously called time of the function
+    let prev = 0;
+    return (...args) => {
+
+        let now = new Date().getTime();
+
+
+        console.log(now - prev, delay);
+
+
+        if (now - prev > delay) {
+            prev = now;
+
+
+            return func(...args);
+        }
     }
-    else {
-        currentIndex++
-    }
-    imagefile.src = arrOfImages[currentIndex]
-    paragraph.textContent = arrOfParagraph[currentIndex]
-    name.textContent = arrOfNames[currentIndex]
-    profession.textContent = arrOfProfession[currentIndex]
 }
-setInterval(() => { automaticCustomerReviewChanger() }, 3000)
+document.body.addEventListener("mousemove", throttleFunction((details) => {
+    let main = document.querySelector('#main')
+    let div = document.createElement("div")
+    div.style.left = details.clientX + "px"
+    div.style.top = details.clientY + "px"
+    let img = document.createElement("img")
+    let arrOfImages = ["imageF/1.jpg", "imageF/2.jpg", "imageF/3.jpg", "imageF/4.jpg", "imageF/5.jpg"]
+    let randomImage = arrOfImages[Math.floor(Math.random() * arrOfImages.length)]
+    img.setAttribute("src",randomImage)
+    div.append(img)
+    div.classList.add("imageFile")
+    document.body.append(div)
 
-
-function rightarrow() {
-    automaticCustomerReviewChanger()
-}
-function leftarrow() {
-    let paragraph = document.getElementById("paragraph")
-    let imagefile = document.getElementById('imagefile')
-    let name = document.getElementById("name")
-    let profession = document.getElementById("profession")
-
-    let arrOfImages = ["secondpageImg/2.png", "secondpageImg/3.png", "secondpageImg/1.png"]
-    let arrOfParagraph = ["Earlier, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Later, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Getter, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato .",]
-    let arrOfNames = ["Jasmeet Singh", "Raghudeep Singh", "Pardeep Singh"]
-    let arrOfProfession = ["Owner - Pick and Move, Udaipur", "Owner - Run and Eat, Udaipur", "Owner - Lyrically and Musically, Udaipur"]
-
-
-    if (currentIndex == 0) {
-        currentIndex = 2
-    }
-    else {
-        currentIndex--
-    }
-    imagefile.src = arrOfImages[currentIndex]
-    paragraph.textContent = arrOfParagraph[currentIndex]
-    name.textContent = arrOfNames[currentIndex]
-    profession.textContent = arrOfProfession[currentIndex]
-}
-
-function coloringHeader() {
-    let selectingHeader = document.querySelectorAll("#confessing #confessinginfo #details h2")
-
-
-    if (currentIndexForHeader > selectingHeader.length - 1) {
-        currentIndexForHeader = 0
-    }
-    else {
-        currentIndexForHeader++
-    }
-    selectingHeader[currentIndexForHeader].style.color = "blue"
-    setTimeout(() => {
-        selectingHeader[currentIndexForHeader].style.color = "black"
-    }, 1000)
-
-}
-
-
-function imageMove() {
-    let imageLaptop = document.getElementById("imageLaptop")
-    let arrOfImages = ["secondpageImg/laptop1.png", "secondpageImg/laptop2.png"]
-
-
-    if (currentIndex > arrOfImages.length - 1) {
-        currentIndex = 0
-    }
-    else {
-        currentIndex++
-    }
-    imageLaptop.setAttribute("src", arrOfImages[currentIndex])
-}
-
-function showNavIcons() {
-    let menu = document.getElementById("menu")
-    let selectingNav = document.querySelector("#backimg nav")
-    let selectingItags = document.querySelectorAll("#backimg nav i")
-    let close = document.getElementById("close")
-
-    let anchorNav = document.querySelectorAll("nav a")
-    anchorNav.forEach((elem) => {
-        elem.style.display = "block"
-
-    })
-
-
-    selectingNav.style.height = "100vh"
-    selectingNav.style.flexDirection = "column"
-    selectingNav.style.backgroundColor = "black"
-    selectingNav.style.position = "absolute"
-    menu.style.display = "none"
-    close.style.display = "block"
-}
-function hidEverything() {
-    let menu = document.getElementById("menu")
-    let selectingNav = document.querySelector("#backimg nav")
-    let anchorNav = document.querySelectorAll("nav a")
-    let close = document.getElementById("close")
-    anchorNav.forEach((elem) => {
-        elem.style.display = "none"
-    })
-    selectingNav.style.height = "0vh"
-    selectingNav.style.flexDirection = "row"
-    selectingNav.style.backgroundColor = "transparent"
-    selectingNav.style.position = "relative"
-    close.style.display = "none"
-    menu.style.display = "block"
-
-}
+    setTimeout(() =>{
+        div.remove()
+    },700)
+}, 100));
 
 
 
+// let currentIndexForHeader = 0
+// let currentIndex = 0
+
+// function automaticCustomerReviewChanger() {
+//     let paragraph = document.getElementById("paragraph")
+//     let imagefile = document.getElementById('imagefile')
+//     let name = document.getElementById("name")
+//     let profession = document.getElementById("profession")
+
+//     let arrOfImages = ["secondpageImg/2.png", "secondpageImg/3.png", "secondpageImg/1.png"]
+//     let arrOfParagraph = ["Earlier, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Later, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Getter, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato .",]
+//     let arrOfNames = ["Jasmeet Singh", "Raghudeep Singh", "Pardeep Singh"]
+//     let arrOfProfession = ["Owner - Pick and Move, Udaipur", "Owner - Run and Eat, Udaipur", "Owner - Lyrically and Musically, Udaipur"]
+
+//     if (currentIndex >= 2) {
+//         currentIndex = 0
+//     }
+//     else {
+//         currentIndex++
+//     }
+//     imagefile.src = arrOfImages[currentIndex]
+//     paragraph.textContent = arrOfParagraph[currentIndex]
+//     name.textContent = arrOfNames[currentIndex]
+//     profession.textContent = arrOfProfession[currentIndex]
+// }
+// setInterval(() => { automaticCustomerReviewChanger() }, 3000)
+
+
+// function rightarrow() {
+//     automaticCustomerReviewChanger()
+// }
+// function leftarrow() {
+//     let paragraph = document.getElementById("paragraph")
+//     let imagefile = document.getElementById('imagefile')
+//     let name = document.getElementById("name")
+//     let profession = document.getElementById("profession")
+
+//     let arrOfImages = ["secondpageImg/2.png", "secondpageImg/3.png", "secondpageImg/1.png"]
+//     let arrOfParagraph = ["Earlier, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Later, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato app.", "Getter, we delivered food orders to our known and repeat customers but now we have been able to create a completely new segment of repeat users - all because of the Zomato .",]
+//     let arrOfNames = ["Jasmeet Singh", "Raghudeep Singh", "Pardeep Singh"]
+//     let arrOfProfession = ["Owner - Pick and Move, Udaipur", "Owner - Run and Eat, Udaipur", "Owner - Lyrically and Musically, Udaipur"]
+
+
+//     if (currentIndex == 0) {
+//         currentIndex = 2
+//     }
+//     else {
+//         currentIndex--
+//     }
+//     imagefile.src = arrOfImages[currentIndex]
+//     paragraph.textContent = arrOfParagraph[currentIndex]
+//     name.textContent = arrOfNames[currentIndex]
+//     profession.textContent = arrOfProfession[currentIndex]
+// }
+
+// function coloringHeader() {
+//     let selectingHeader = document.querySelectorAll("#confessing #confessinginfo #details h2")
+
+
+//     if (currentIndexForHeader > selectingHeader.length - 1) {
+//         currentIndexForHeader = 0
+//     }
+//     else {
+//         currentIndexForHeader++
+//     }
+//     selectingHeader[currentIndexForHeader].style.color = "blue"
+//     setTimeout(() => {
+//         selectingHeader[currentIndexForHeader].style.color = "black"
+//     }, 1000)
+
+// }
+
+
+// function imageMove() {
+//     let imageLaptop = document.getElementById("imageLaptop")
+//     let arrOfImages = ["secondpageImg/laptop1.png", "secondpageImg/laptop2.png"]
+
+
+//     if (currentIndex > arrOfImages.length - 1) {
+//         currentIndex = 0
+//     }
+//     else {
+//         currentIndex++
+//     }
+//     imageLaptop.setAttribute("src", arrOfImages[currentIndex])
+// }
+
+// function showNavIcons() {
+//     let menu = document.getElementById("menu")
+//     let selectingNav = document.querySelector("#backimg nav")
+//     let selectingItags = document.querySelectorAll("#backimg nav i")
+//     let close = document.getElementById("close")
+
+//     let anchorNav = document.querySelectorAll("nav a")
+//     anchorNav.forEach((elem) => {
+//         elem.style.display = "block"
+
+//     })
+
+
+//     selectingNav.style.height = "100vh"
+//     selectingNav.style.flexDirection = "column"
+//     selectingNav.style.backgroundColor = "black"
+//     selectingNav.style.position = "absolute"
+//     menu.style.display = "none"
+//     close.style.display = "block"
+// }
+// function hidEverything() {
+//     let menu = document.getElementById("menu")
+//     let selectingNav = document.querySelector("#backimg nav")
+//     let anchorNav = document.querySelectorAll("nav a")
+//     let close = document.getElementById("close")
+//     anchorNav.forEach((elem) => {
+//         elem.style.display = "none"
+//     })
+//     selectingNav.style.height = "0vh"
+//     selectingNav.style.flexDirection = "row"
+//     selectingNav.style.backgroundColor = "transparent"
+//     selectingNav.style.position = "relative"
+//     close.style.display = "none"
+//     menu.style.display = "block"
+
+// }
 
 
 
@@ -143,16 +183,19 @@ function hidEverything() {
 
 
 
-setInterval(() => {
-    coloringHeader()
-}, 2000)
 
-setInterval(() => {
-    imageMove()
-}, 2000)
-setInterval(() => {
 
-    automaticCustomerReviewChanger()
 
-}, 3000)
+// setInterval(() => {
+//     coloringHeader()
+// }, 2000)
+
+// setInterval(() => {
+//     imageMove()
+// }, 2000)
+// setInterval(() => {
+
+//     automaticCustomerReviewChanger()
+
+// }, 3000)
 
